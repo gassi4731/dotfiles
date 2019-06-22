b709467706bd33647fdda78ebb75117cac1f8d15
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2019-06-23 00:09:40 +0900

    Add

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 
 # add submodule
 git submodule update --init --recursive
+
+# prezto
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
 
 # symlink dotfiles
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
