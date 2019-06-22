403e92fde7b7038b45efa007ae7173c8c35fc083
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2019-06-22 23:33:31 +0900

    Modify install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# symlink pure
-#ln -s "$PWD/zsh.d/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
-#ln -s "$PWD/zsh.d/async.zsh" /usr/local/share/zsh/site-functions/async
+# add submodule
+git submodule update --init --recursive
 
 # symlink dotfiles
-#ln -sf ~/dotfiles/.vimrc ~/.vimrc
-#ln -sf ~/dotfiles/.zshrc ~/.zshrc
-#ln -sf ~/dotfiles/.vim ~/.vim
-#chsh -s $(which zsh)
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/.vim ~/.vim
 
-# add neobundle
-#git submodule add https://github.com/Shougo/neobundle.vim.git .vim/bundle/neobundle.vim
+# change shell
+chsh -s $(which zsh)
+
